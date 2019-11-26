dce374d2f68eaee4d2dac1cf69d2df81d0d583b9
calderon <calderon@cern.ch>
2019-11-26 21:14:34 +0100

    update reco sequence

--- clients/mutracking_dqm_sourceclient-live_cfg.py
+++ clients/mutracking_dqm_sourceclient-live_cfg.py
@@ -18,7 +18,28 @@ if (live):
     process.load("DQM.Integration.config.inputsource_cfi")
 
 # for testing in lxplus
-elif(offlineTesting):---
+elif(offlineTesting):
+    process.load("DQM.Integration.config.fileinputsource_cfi")
+    
+ 
+print("Running with run type = ", process.runType.getRunType())
+
+if (process.runType.getRunType() != process.runType.cosmic_run):
+    print("MuTracking client runs only in cosmics, disabling")
+    
+
+#----------------------------
+#### DQM Environment
+#----------------------------
+
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+process.load("DQM.Integration.config.environment_cfi")
+
+
+#----------------------------
+#### DQM Live Environment
+#----------------------------
 process.dqmEnv.subSystemFolder = 'Muons'
 process.dqmSaver.tag = 'Muons'
 ##?? process.dqmSaver.backupLumiCount = 30
@@ -58,7 +79,16 @@ if (live):
     process.load("DQM.Integration.config.FrontierCondition_GT_cfi")
 
 # Condition for lxplus: change and possibly customise the GT
-elif(offlineTesting):-----------------------------
+elif(offlineTesting):
+    process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+    from Configuration.AlCa.GlobalTag import GlobalTag as gtCustomise
+    process.GlobalTag = gtCustomise(process.GlobalTag, 'auto:run2_data', '')
+
+
+
+#------------------------------------                                                                                              
+# Cosmic muons reconstruction modules
+#------------------------------------
 
 #1 RAW-TO-DIGI 
 
@@ -66,16 +96,35 @@ process.muRawToDigi = cms.Sequence(process.L1TRawToDigi +
                                    process.muonCSCDigis +
                                    process.muonDTDigis +
                                    process.muonRPCDigis +
-                                   process.muonRPCNewDigis +
                                    process.muonGEMDigis)
                                    
 
 
-#2 STA RECO
+#2 LOCAL RECO
+from RecoLocalMuon.Configuration.RecoLocalMuonCosmics_cff import *
+from RecoLocalMuon.RPCRecHit.rpcRecHits_cfi import *
 
-## From  cmssw/RecoMuon/Configuration/python/RecoMuonCosmics_cff.py 
+process.dtlocalreco = cms.Sequence(dt1DRecHits*dt4DSegments)
+process.csclocalreco = cms.Sequence(csc2DRecHits*cscSegments)
+process.muLocalRecoCosmics = cms.Sequence(process.dtlocalreco+process.csclocalreco+process.rpcRecHits)
 
-process.muSTAreco = cms.Sequence(process.STAmuontrackingforcosmics)
+
+#3 STA RECO 
+
+from RecoMuon.MuonSeedGenerator.CosmicMuonSeedProducer_cfi import *
+from RecoMuon.CosmicMuonProducer.cosmicMuons_cff import *
+
+##Reco Beam Spot from DB 
+from RecoVertex.BeamSpotProducer.BeamSpotFakeParameters_cfi import *
+process.beamspot = cms.EDAnalyzer("BeamSpotFromDB")
+process.offlineBeamSpot = cms.EDProducer("BeamSpotProducer")
+process.beampath = cms.Sequence(process.beamspot+process.offlineBeamSpot)
+
+
+process.muSTAreco = cms.Sequence(process.CosmicMuonSeed*process.cosmicMuons)
+
+
+
 
 
 #--------------------------                                                                                                         # Service                                                                                                                           #--------------------------                                                                                                                      
@@ -111,8 +160,7 @@ process.muonDQM = cms.Sequence(process.muonCosmicAnalyzer)
 # Scheduling
 #--------------------------
 
-process.allReco = cms.Sequence(process.muRawToDigi*process.muSTAreco)
-
+process.allReco = cms.Sequence(process.muRawToDigi*process.muLocalRecoCosmics*process.beampath*process.muSTAreco)
 
 process.allPaths = cms.Path(process.hltHighLevel * 
                             process.hltTriggerTypeFilter * 
